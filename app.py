371ec04dcce93f8f7eca9dc7a4a82f96e864661a
4444fede <federico.vega@gm2dev.com>
2024-10-09 22:01:56 -0300

    feat algo

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def profile():
 
 @app.route('/match')
 def match():
-    return render_template('match.html')    
+    return render_template('match.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
